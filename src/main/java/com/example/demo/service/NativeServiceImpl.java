c127fe7e2b2b5623444f40e08c9e89166520d966
Aimberê Galdino <biroska@gmail.com>
2023-08-26 14:36:40 -0300

    Composicao profiles com on-profile

--- src/main/java/com/example/demo/service/NativeServiceImpl.java
+++ src/main/java/com/example/demo/service/NativeServiceImpl.java
@@ -1,13 +1,22 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
 
 @Service
 @Profile("!local")
 public class NativeServiceImpl implements NativeService {
+
+    @Value("${application.version}")
+    private String version;
+
+    @Value("${api.top-rated}")
+    private String topRated;
     @Override
     public String nativeProfile() {
-        return "Hello World";
+        return MessageFormat.format("Hello World! {0} - {1}", version, topRated.substring(0,10));
     }
 }
--- src/main/java/com/example/demo/service/NativeServiceLocalImpl.java
+++ src/main/java/com/example/demo/service/NativeServiceLocalImpl.java
@@ -1,13 +1,24 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
 
 @Service
 @Profile("local")
 public class NativeServiceLocalImpl implements NativeService {
+
+
+    @Value("${application.version}")
+    private String version;
+
+    @Value("${api.top-rated}")
+    private String topRated;
+
     @Override
     public String nativeProfile() {
-        return "Hello World - Local";
+        return MessageFormat.format("Hello World! {0} - {1}", version, topRated.substring(0,10));
     }
 }
